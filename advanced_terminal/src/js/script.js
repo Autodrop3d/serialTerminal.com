ef4d424d2fabd6d5cb2fadb46715fe18b2239cb7
DaOfficialWizard <pyr0ndet0s97@gmail.com>
2022-06-30 01:29:33 +0200

    setting up file structure

---
+++ advanced_terminal/src/js/script.js
@@ -0,0 +1,118 @@
+var port,
+  textEncoder,
+  writableStreamClosed,
+  writer,
+  historyIndex = -1;
+const lineHistory = [];
+
+async function connectSerial() {
+  try {
+    // Prompt user to select any serial port.
+    port = await navigator.serial.requestPort();
+    await port.open({ baudRate: document.getElementById("baud").value });
+    await port.setSignals({
+      dataTerminalReady: document.getElementById("rtsOn").value,
+      requestToSend: document.getElementById("dtrOn").value,
+    });
+    listenToPort();
+
+    textEncoder = new TextEncoderStream();
+    writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+
+    writer = textEncoder.writable.getWriter();
+  } catch {
+    alert("Serial Connection Failed");
+  }
+}
+
+async function sendCharacterNumber() {
+  document.getElementById("lineToSend").value = String.fromCharCode(
+    document.getElementById("lineToSend").value
+  );
+}
+
+async function sendSerialLine() {
+  dataToSend = document.getElementById("lineToSend").value;
+  lineHistory.unshift(dataToSend);
+  historyIndex = -1; // No history entry selected
+  if (document.getElementById("carriageReturn").checked == true)
+    dataToSend = dataToSend + "\r";
+  if (document.getElementById("addLine").checked == true)
+    dataToSend = dataToSend + "\n";
+  if (document.getElementById("echoOn").checked == true)
+    appendToTerminal("> " + dataToSend);
+  await writer.write(dataToSend);
+  document.getElementById("lineToSend").value = "";
+  //await writer.releaseLock();
+}
+
+async function listenToPort() {
+  const textDecoder = new TextDecoderStream();
+  const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+  const reader = textDecoder.readable.getReader();
+
+  // Listen to data coming from the serial device.
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done) {
+      // Allow the serial port to be closed later.
+      //reader.releaseLock();
+      break;
+    }
+    // value is a string.
+    appendToTerminal(value);
+  }
+}
+
+const serialResultsDiv = document.getElementById("serialResults");
+
+async function appendToTerminal(newStuff) {
+  serialResultsDiv.innerHTML += newStuff;
+  if (serialResultsDiv.innerHTML.length > 3000)
+    serialResultsDiv.innerHTML = serialResultsDiv.innerHTML.slice(
+      serialResultsDiv.innerHTML.length - 3000
+    );
+
+  //scroll down to bottom of div
+  serialResultsDiv.scrollTop = serialResultsDiv.scrollHeight;
+}
+
+function scrollHistory(direction) {
+  // Clamp the value between -1 and history length
+  historyIndex = Math.max(
+    Math.min(historyIndex + direction, lineHistory.length - 1),
+    -1
+  );
+  if (historyIndex >= 0) {
+    document.getElementById("lineToSend").value = lineHistory[historyIndex];
+  } else {
+    document.getElementById("lineToSend").value = "";
+  }
+}
+
+document
+  .getElementById("lineToSend")
+  .addEventListener("keyup", async function (event) {
+    if (event.keyCode === 13) {
+      sendSerialLine();
+    } else if (event.keyCode === 38) {
+      // Key up
+      scrollHistory(1);
+    } else if (event.keyCode === 40) {
+      // Key down
+      scrollHistory(-1);
+    }
+  });
+
+document.getElementById("baud").value =
+  localStorage.baud == undefined ? 9600 : localStorage.baud;
+document.getElementById("addLine").checked =
+  localStorage.addLine == "false" ? false : true;
+document.getElementById("carriageReturn").checked =
+  localStorage.carriageReturn == "false" ? false : true;
+document.getElementById("echoOn").checked =
+  localStorage.echoOn == "false" ? false : true;
+document.getElementById("rtsOn").checked =
+  localStorage.rtsOn == "false" ? false : true;
+document.getElementById("dtrOn").checked =
+  localStorage.dtrOn == "false" ? false : true;
